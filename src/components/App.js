5c743a9332dda3521314eac6c9e1a81c37b90819
abc <abc@example.com>
2021-12-16 18:47:01 -0700

    I finished the routes

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import styled from 'styled-components';
 import Header from './Header';
 import BloomHeader from './BloomHeader';
 import Login from './Login';
+import View from './View';
+import Logout from './Logout';
 
 const App = () => {
   return (
@@ -12,6 +14,15 @@ const App = () => {
       <BloomHeader/>
       <Header/>
       <RouteContainer>
+        <Route path="/view">
+          <View/>
+        </Route>
+        <Route path="/logout">
+          <Logout/>
+        </Route>    
+        <Route path="/login">
+          <Login/>
+        </Route>    
         <Route exact path="/">
           <Login/>
         </Route>          
